d361f1d008b47ce22034c18e92bf2bd85f3ccd99
Lukas Koch <lukas.koch@mailbox.org>
2024-08-20 15:50:32 +0200

    Fix up distributions code

--- src/nustattools/stats/__init__.py
+++ src/nustattools/stats/__init__.py
@@ -8,52 +8,7 @@ Statistical distributions that are useful, but not available in
 
 from __future__ import annotations
 
-import numpy as np
+from . import _dist
+from ._dist import *  # noqa: F403
 
-from scipy.stats import chi, chi2, rv_continuous
-
-__all__ = ["Bee", "Bee2", "Cee", "Cee2"]
-
-
-class Bee(rv_continuous):
-    def __init__(self, df=1):
-        super().__init__(a=0)
-        self.df = df
-        self.chi = chi(df=1)
-
-    def _cdf(self, x):
-        p = self.chi.cdf(x) ** self.df
-        return p
-
-
-class Bee2(rv_continuous):
-    def __init__(self, df=1):
-        super().__init__(a=0)
-        self.df = df
-        self.chi2 = chi2(df=1)
-
-    def _cdf(self, x):
-        p = self.chi2.cdf(x) ** self.df
-        return p
-
-
-class Cee(rv_continuous):
-    def __init__(self, k=[1]):
-        super().__init__(a=0)
-        self.k = np.atleast_1d(k)
-        self.chi = [chi(df=_df) for _df in self.k]
-
-    def _cdf(self, x):
-        p = [c.cdf(x) for c in self.chi]
-        return np.prod(p, axis=0)
-
-
-class Cee2(rv_continuous):
-    def __init__(self, k=[1]):
-        super().__init__(a=0)
-        self.k = np.atleast_1d(k)
-        self.chi2 = [chi2(df=_df) for _df in self.k]
-
-    def _cdf(self, x):
-        p = [c.cdf(x) for c in self.chi2]
-        return np.prod(p, axis=0)
+__all__ = _dist.__all__
