297af9adc0c846e98bd792c280d09bfa07edb863
Lukas Koch <lukas.koch@mailbox.org>
2025-10-24 12:59:25 +0200

    Increase coverage.

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import numpy as np
+import pytest
 from matplotlib import pyplot as plt
 
 import nustattools.plotting as p
@@ -13,5 +14,13 @@ def test_hinton():
     p.hinton(M, origin="lower")
     p.hinton(M, cmap="gray")
     p.hinton(M, legend=True)
+    for s in ("circle", "square"):
+        p.hinton(M, shape=s)
+    with pytest.raises(ValueError, match="Unknown shape"):
+        p.hinton(M, shape="")
+    for o in ("upper", "lower"):
+        p.hinton(M, origin=o)
+    with pytest.raises(ValueError, match="Unknown origin"):
+        p.hinton(M, origin="")
     fig, ax = plt.subplots()
     p.hinton(M, ax=ax)
